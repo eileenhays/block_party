bdebc0d7475ff45459c32287d35c95675a9f5272
vagrant <vagrant@vagrant.vm>
2017-10-03 01:04:43 +0000

    jsonify event list to use with JS and added more properties to events

--- server.py
+++ server.py
@@ -50,45 +50,57 @@ def search_for_events():
     url = 'https://api.meetup.com/2/open_events'
     response = requests.get(url, params=payload)
     data = response.json()
-    event_list = search_events(data)
 
-    return render_template("map.html", event_list=event_list)
+    return jsonify_events(data)
+    # return render_template("map.html", map_events=jsonify_events(data))
 
-@app.route('/render-test')
-def render_test_template():
-    """Renders the test.html file for testing purposes."""
 
-    return render_template('test.html')
+# @app.route('/render-test')
+# def render_test_template():
+#     """Renders the test.html file for testing purposes."""
+
+#     return render_template('test.html', events=events)
+
+# From Javascript place object: 
+# places.geometry.location.lat()
+# 37.7893921
+# places.geometry.location.lng()
+# -122.40775389999999
+# places.adr_address
+# places.formatted_address
 
 
 #******************* HELPER METHODS ****************************
-def search_events(data):
-    """"Parses out relevant data from the Meetup API response.
-    Name, description, url, lat, long, and rsvp_num."""
+def jsonify_events(data):
+    """"Parses out relevant data from the Meetup API response, and 
+    dumps clean event info into JSON."""
 
     events_list = data['results']
 
-    map_events = []
-
+    map_events = {}
+    #new dictionary created for every event
     for event in events_list:
         event_dict = {}
+
         event_dict['name'] = event['name']
         event_dict['description'] = event['description']
         event_dict['time'] = event['time']
         event_dict['utc_offset'] = event['utc_offset']    
         event_dict['url'] = event['event_url']
+        event_dict['rsvp_num'] = event['yes_rsvp_count']        
         if 'venue' in event:
             event_dict['lat'] = event['venue']['lat']
             event_dict['lng'] = event['venue']['lon']
         else: #check if group is the actual default location
             event_dict['lat'] = event['group']['group_lat']
             event_dict['lng'] = event['group']['group_lon']
-        event_dict['rsvp_num'] = event['yes_rsvp_count']
 
-        map_events.append(event_dict)
+        evt_id = event['id']
+        map_events[evt_id] = event_dict #add each to map_events
 
-    return map_events
+    # events_json = jsonify(map_events)
 
+    return json.dumps(map_events)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
